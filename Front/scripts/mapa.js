b33b49e93ef7c2d16d97ff1d348e1dde857e9e69
Aimar Gende Arruabarrena <agendearru@unav.es>
2024-04-22 10:45:33 +0200

    titulo de pagina cambiado, toggle de info de manifa puesto. Arreglar que mapa no cargue

--- Front/scripts/mapa.js
+++ Front/scripts/mapa.js
@@ -1,9 +1,32 @@
-var locationMap = L.map('locations-map').setView([43.316893, -1.980888], 10);//Mapa para escoger localizacion
+var locationMap = L.map('locations-map', {
+    center: [43.316893, -1.980888],
+    zoom: 10,
+});//Mapa para escoger localizacion
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(locationMap);
- 
+
+let marker = L.marker([43.316893, -1.980888]).addTo(locationMap);
+marker._icon.classList.add('marcador')
+marker.on('click', () => {
+    toggleInfo('info');
+});
+
 var routesMap = L.map('rutas-bloqueadas').setView([43.316893, -1.980888], 7);//Mapa para mostrar ruta de manifestacion
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(routesMap);
+
+function toggleInfo(info) {
+    let infoContainer = document.getElementById('info-container');
+    infoContainer.classList.toggle('hidden');
+    let buscadorContainer = document.getElementById('buscador-container');
+    buscadorContainer.classList.toggle('hidden');
+    let map = document.getElementById('locations-map');
+    map.classList.toggle('hidden');
+    changeInfo(info);
+}
+
+function changeInfo(info) {
+    console.log(info)
+}

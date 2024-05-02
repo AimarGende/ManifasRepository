da0943dca21d14eb6ec268b100e95fe83dbaef7d
Aimar Gende Arruabarrena <agendearru@unav.es>
2024-05-02 09:13:16 +0200

    Menu hecho en index.html

---
+++ Front/scripts/toggleMenu.js
@@ -0,0 +1,16 @@
+function toggleMenu(page) {
+    if (page.className.includes('active')) {
+        return;
+    }
+    else {
+        let menuItems = document.getElementsByClassName('menu-item')
+        for (let menuItem of menuItems) {
+            if (menuItem.className.includes('active')) {
+                menuItem.classList.toggle('active')
+                document.getElementById(menuItem.textContent.toLowerCase()).style.display = 'none';
+            }
+        }
+        page.classList.toggle('active');
+        document.getElementById(page.textContent.toLowerCase()).style.display = 'block';
+    }
+}
